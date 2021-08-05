073e731b00bb9c566b8b9996bbdf169a4b630b7a
DROW900 <cmunozc1500@alumno.ipn.mx>
2021-08-05 17:05:36 -0500

    Correccion

--- app.js
+++ app.js
@@ -15,12 +15,8 @@ const vistaRoles = require('./app/vistas/vista.roles');
 const vistaUsuarios = require('./app/vistas/vista.usuarios');
 const vistaProductos = require('./app/vistas/vista.productos');
 const vistaCategorias = require('./app/vistas/vista.categorias');
-<<<<<<< HEAD
-
-
-=======
 const vistaInterfaz = require('./app/vistas/vista.interfaz')
->>>>>>> 61108696adf7ef58d2f3b112dfd5dd6646b3aa01
+
 //Middleware globales
 app.use(express.json())
 app.use(cors())
@@ -57,6 +53,6 @@ inicioServer();
 //Rutas a llamar
 vistaRoles(app)
 vistaUsuarios(app)
-
+vistaInterfaz(app)
 vistaProductos(app)
 vistaCategorias(app)
