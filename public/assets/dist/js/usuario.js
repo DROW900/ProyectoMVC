98db08b91c54dcd4d03e3624a87fba38a711ee76
KevinnAlvarezz <kevinalvarez417@aragon.unam.mx>
2021-08-06 19:48:49 -0500

    modifique a puerto 3000

--- public/assets/dist/js/usuario.js
+++ public/assets/dist/js/usuario.js
@@ -8,7 +8,7 @@ class Usuario {
             alert('Alguno de los campos está vacío')
             return
         }
-        let resultado = await fetch('http://localhost:400/login', {
+        let resultado = await fetch('http://localhost:3000/login', {
             method: 'post',
             headers: {
                 "Accept": "application/json, text/plain, */*",
@@ -24,7 +24,7 @@ class Usuario {
         this.token = datos.token;
         this.tipo_rol = datos.tipo_rol;
         if (this.token != undefined && this.tipo_rol == 1) {
-            window.location.href = 'http://localhost:400/admin_usuarios'
+            window.location.href = 'http://localhost:3000/admin_usuarios'
         } else if (this.token != undefined && this.tipo_rol == 2) {
             window.location.href = 'http://localhost:400/principal'
         }
@@ -56,7 +56,7 @@ class Usuario {
     static async eliminar(id) {
         //localStorage.setItem('id', id)
         let usuario = await this.recuperarUsuario();
-        let resultado = await fetch(`http://localhost:400/usuarios/${id}`, {
+        let resultado = await fetch(`http://localhost:3000/usuarios/${id}`, {
             method: 'delete',
             headers: {
                 "Accept": "application/json, text/plain, */*",
@@ -69,11 +69,11 @@ class Usuario {
 
 
     static async mostrar_form() {
-        window.location.href = 'http://localhost:400/usuario_form'
+        window.location.href = 'http://localhost:3000/usuario_form'
 
     }
     static async primer_registro_form() {
-        window.location.href = 'http://localhost:400/usuario_comun_form'
+        window.location.href = 'http://localhost:3000/usuario_comun_form'
 
     }
     static async registrar() {
@@ -88,7 +88,7 @@ class Usuario {
         let pass = document.getElementById('password').value;
         let tipo_rol = usuario.tipo_rol;
 
-        const rawResponse = await fetch(`http://localhost:400/admin_usuario_registrar/${tipo_rol}`, {
+        const rawResponse = await fetch(`http://localhost:3000/admin_usuario_registrar/${tipo_rol}`, {
             method: 'POST',
             headers: {
                 "Accept": "application/json, text/plain, */*",
@@ -124,7 +124,7 @@ class Usuario {
         let pass = document.getElementById('password').value;
 
 
-        const rawResponse = await fetch(`http://localhost:400/primer_registro`, {
+        const rawResponse = await fetch(`http://localhost:3000/primer_registro`, {
             method: 'POST',
             headers: {
                 "Accept": "application/json, text/plain, */*",
