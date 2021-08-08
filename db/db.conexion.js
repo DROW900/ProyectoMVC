975cd5b53266478646a5a6f0ff0952762e3530c8
KevinnAlvarezz <kevinalvarez417@aragon.unam.mx>
2021-08-08 17:53:05 -0500

    Vista de productos

--- db/db.conexion.js
+++ db/db.conexion.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('proyecto_tecla', null, null, {
+const sequelize = new Sequelize(process.env.DB_NAME, null, null, {
     dialect: 'mssql',
     server: process.env.DB_HOST,
     port: process.env.DB_PORT,
--- db/db.modelo.productos.js
+++ db/db.modelo.productos.js
@@ -1,23 +1,22 @@
-const {DataTypes, Model} = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../db/db.conexion');
 const SubCategorias = require('../db/db.modelo.subCategorias');
 //Se define el modelo de los poductos que se va a utilizar
-class Productos extends Model{}
+class Productos extends Model {}
 
-Productos.init(
-    {
-        //Se definen los campos del modelo
-        nombre: {type: DataTypes.STRING(50), allowNull:false},
-        descripcion: {type: DataTypes.STRING(150), allowNull:false},
-        precio: {type: DataTypes.FLOAT, allowNull:false},
-        disponibilidad: {type: DataTypes.INTEGER, allowNull:false},
-        status: {type:DataTypes.INTEGER, allowNull:false}
-    },
-    {
-        sequelize: sequelize,
-        modelName: 'productos',
-        timestamps: true
-    });
+Productos.init({
+    //Se definen los campos del modelo
+    nombre: { type: DataTypes.STRING(50), allowNull: false },
+    descripcion: { type: DataTypes.STRING(150), allowNull: false },
+    precio: { type: DataTypes.FLOAT, allowNull: false },
+    disponibilidad: { type: DataTypes.INTEGER, allowNull: false },
+    url: { type: DataTypes.STRING, allowNull: false },
+    status: { type: DataTypes.INTEGER, allowNull: false }
+}, {
+    sequelize: sequelize,
+    modelName: 'productos',
+    timestamps: true
+});
 
 Productos.SubCategorias = Productos.belongsTo(SubCategorias);
 module.exports = Productos;
